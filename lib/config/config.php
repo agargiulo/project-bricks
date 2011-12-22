6fd25f1b2c34cf42cb34eb5afcd555ccd4e336a5
Anthony Gargiulo <agargiulo@anthonygargiulo.info>
2011-12-22 01:49:21 -0500

    changed the "base url"

--- lib/config/config.php
+++ lib/config/config.php
@@ -4,10 +4,10 @@
  */
 
 // ex: http://yourdomain.com WITH TRAILING SLASH
-@$config['base_url'] = "http://" .  $_SERVER['HTTP_HOST'].'/';
+@$config['base_url'] = "https://" .  $_SERVER['HTTP_HOST'].'/';
 
 // ex: /sub_dir --> http://yourdomain.com/sub_dir/ WITH TRAILING SLASH
-$config['url_extension'] = 'foundation_php/';
+$config['url_extension'] = '~agargiulo/project-bricks/';
 
 // leave blank for mod_rewrite.
 // otherwise, include trailing slash
